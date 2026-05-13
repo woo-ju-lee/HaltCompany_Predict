f10deb3fdd8d8f772047a51a3ef35813fe319962
wooju <pigboy07@naver.com>
2026-05-13 18:41:33 +0900

    Split train/test/valid

--- Predict_mainline.R
+++ Predict_mainline.R
@@ -16,3 +16,34 @@ library(xgboost)
 
 
 ###동작 라인
+set.seed(123)
+
+firm_ids <- unique(primary_ratio_only$corp_code)
+
+train_firms <- sample(
+  firm_ids,
+  size = floor(length(firm_ids) * 0.8)
+)
+
+test_firms <- setdiff(firm_ids, train_firms)
+
+train <- primary_ratio_only %>%
+  filter(
+    corp_code %in% train_firms,
+    bsns_year >= 2015,
+    bsns_year <= 2021
+  )
+
+valid <- primary_ratio_only %>%
+  filter(
+    corp_code %in% train_firms,
+    bsns_year == 2022
+  )
+
+test <- primary_ratio_only %>%
+  filter(
+    corp_code %in% test_firms,
+    bsns_year >= 2023,
+    bsns_year <= 2024
+  )
+
